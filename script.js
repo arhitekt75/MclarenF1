6eae0525bcd6412b737260b18d0b985e5a784605
arhitekt75 <arhitekt@blic.net>
2023-09-19 22:05:37 +0200

    teams js added

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ let menu_open = document.getElementById('menu-open');
 let menu_close = document.getElementById('menu-close');
 let side_menu = document.getElementById('side-menu');
 let body = document.querySelector('body');
+let teams = document.getElementById('teams');
+let menu_close2 = document.getElementById('menu-close2');
+let btn = document.getElementById('btn');
 
 menu_open.addEventListener('click',openMenu);
 menu_close.addEventListener('click',closeMenu);
+btn.addEventListener('click',teamsOpen);
+menu_close2.addEventListener('click',teamsClose);
 
 function openMenu(){
     side_menu.style.display = 'block';
@@ -14,4 +19,13 @@ function openMenu(){
 function closeMenu(){
     side_menu.style.display = 'none';
     body.style.overflowY = 'visible';
+}
+
+function teamsOpen() {
+    teams.style.display='block';
+  
+}
+
+function teamsClose() {
+    teams.style.display='none';
 }
